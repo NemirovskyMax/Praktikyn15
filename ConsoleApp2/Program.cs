f128dd547a725ec55a7cc94d5419b2da6e4c0218
NemirovskyMax <artemjaik@gmail.com>
2024-12-24 10:20:26 +0300

    3

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Func() 
+        {
+            int count = 0;
+            for (int i = 10; i < 99; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i); count++;
+                    Console.WriteLine($"count={count}");
+
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+    }
+}
