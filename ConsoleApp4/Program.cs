baa969d71bd8c7f59b339d7f7bff864ad57d86f0
NemirovskyMax <artemjaik@gmail.com>
2024-12-24 10:36:09 +0300

    4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Func()
+        {
+            Random rnd = new Random();
+            for (uint x = 1; x <= 10; x++)
+            {
+                int number = rnd.Next(-40,+40);
+                Console.WriteLine($"{x}l число{number}");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+
+        }
+    }
+}
