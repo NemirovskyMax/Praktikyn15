e9a04457410e03616590a8e1c89e1e13d1c82068
NemirovskyMax <artemjaik@gmail.com>
2024-12-24 09:58:34 +0300

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{   /// <summary>
+    /// Напечатать ряд чисел 20 в виде: 20 20 20 20 20 20 20 20 20 20.
+    /// </summary>
+    internal class Program
+    {
+        static void Fou()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{20}\t");
+        }   }
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
